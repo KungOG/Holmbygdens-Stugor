c17ff7b57adfe67f70d63cb25e582aa82c6b23ff
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-03-08 20:29:58 +0100

    starting to fix the admin site and make it possible to add a cabin

--- Holmbygdens-Stugor/src/views/Admin.vue
+++ Holmbygdens-Stugor/src/views/Admin.vue
@@ -1,7 +1,19 @@
 <template>
-    <main class='content'>
-    <section>
+    <main class="content">
+    <section class="cabin">
         <h1>Admin site</h1>
+    </section>
+    <section class="addCabin">
+        <input type="text" placeholder="namn" v-model="">
+        <input type="number" placeholder="pris" v-model="">
+        <input type="text" placeholder="stugområde" v-model="">
+        <input type="text" placeholder="var adress" v-model="">
+        <input type="text" placeholder="datum från" v-model="">
+        <input type="text" placeholder="datum till" v-model="">
+        <input type="text" placeholder="otillgänliga" v-model="">
+        <input type="text" placeholder="tillgängliga" v-model="">
+        <input type="text" placeholder="info" v-model="">
+        <a href="#" class="btn">Lägg till Stuga</a>
     </section>
     <router-view />
     </main>
@@ -9,7 +21,31 @@
 
 <script>
 export default {
-    name : 'admin'
+    name : 'admin',
+    beforeMount () {
+        this.$store.dispatch('getCabin');
+    },
+    data () {
+        return {
+            name: "",
+            info: "",
+            price: "",
+            where: {
+                city: "",
+                adress: ""
+            },
+            date: {
+                from: "",
+                to: "",
+                checkin: "15.00",
+                checkout: "10.00"
+            },
+            cabins: {
+                available: "",
+                unavailable: ""
+            }
+        }
+    }
 }
 </script>
 
