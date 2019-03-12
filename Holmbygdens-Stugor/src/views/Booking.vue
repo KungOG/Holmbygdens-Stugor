c8e222ab3f92c19f80d97d72df7a6a3cbe893421
Schavon <Alexandra.schavon@hotmail.com>
2019-03-12 15:22:04 +0100

    Date/month Picker

--- Holmbygdens-Stugor/src/views/Booking.vue
+++ Holmbygdens-Stugor/src/views/Booking.vue
@@ -1,15 +1,39 @@
 <template>
     <main class='content'>
         <h1>Booking Site</h1>
+
+    <date-picker v-model="range" lang="en" range type="date" format="YYYY-MM-dd"
+    width="500"></date-picker>
+
+    <button type="button" onclick="alert('Hello world!')">Click Me!</button>
     </main>
 </template>
 
 <script>
+import axios from 'axios'
+import DatePicker from 'vue2-datepicker'
+
 export default {
-    name : 'booking'
+    name : 'App',
+    components: {
+       DatePicker
+    },
+    data() {
+    return {
+    date: '',
+    time: '',
+    timePickerOptions:{
+    start: '00:00',
+    step: '00:30',
+    end: '23:30'
+    },
+    datetime: '',
+    range: ''
+
+    }
+    }
 }
 </script>
 
 <style lang="scss">
-
 </style>
