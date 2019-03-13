fc52e0578b6525b1b17ed36a5da9a336f6fa4e98
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-03-13 19:19:24 +0100

    starting to make the delete part on the mongodb

---
+++ Holmbygdens-Stugor/src/components/Bookings.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <section>
+            <h1>oaihdj</h1>
+        </section>
+    </main>
+</template>
+
+<script>
+export default {
+    name : 'booking'
+}
+</script>
+
+<style lang="scss">
+
+</style>
