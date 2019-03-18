1005f14954801915c693341af75ce043dad9b1b9
Schavon <Alexandra.schavon@hotmail.com>
2019-03-18 14:38:49 +0100

    filtrera cabins

--- Holmbygdens-Stugor/src/views/Booking.vue
+++ Holmbygdens-Stugor/src/views/Booking.vue
@@ -3,7 +3,13 @@
     <main class="content">
         <section class="cabin">
         <h1>Val av Stuga</h1>
-        <tr v-for="cabin in cabins" :key="cabin._id" :cabin="cabin" @click="$router.push(`/booking/${cabin._id}`)">
+
+        <section id="input">
+      <input type="text" v-model="search" placeholder="Filter results" />
+    </section>
+
+
+        <tr v-for="cabin in filteredCabins" :key="cabin._id" :cabin="cabin" @click="$router.push(`/booking/${cabin._id}`)">
             <td>{{cabin.name}}</td>
             <td>{{cabin.price}}</td>
             <td>{{cabin.info}}</td>
@@ -16,6 +22,11 @@
 <script>
 export default {
     name : 'booking',
+    data(){
+      return {
+        search: ""
+      }
+    },
     beforeMount () {
         /* Innan sidan laddas, ska den först hämta våra stugor */
         this.$store.dispatch('getCabin');
@@ -24,11 +35,20 @@ export default {
         cabins () {
             /* Hämta alla stugor */
             return this.$store.state.cabins;
-        }
+        },
+        filteredCabins(){
+            return this.cabins.filter((cabin) => {
+              return cabin.name.match(this.search);
+            })
+          }
     }
 }
 </script>
 
 <style lang="scss">
+
+.choose {
+border-style: solid;
+}
 
 </style>
