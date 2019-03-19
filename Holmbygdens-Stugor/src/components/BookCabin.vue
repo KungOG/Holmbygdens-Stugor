3d0aa2129284e036c3d6c1da0af998ef22d7aae8
Schavon <Alexandra.schavon@hotmail.com>
2019-03-19 19:40:49 +0100

    undefined name

--- Holmbygdens-Stugor/src/components/BookCabin.vue
+++ Holmbygdens-Stugor/src/components/BookCabin.vue
@@ -1,24 +1,24 @@
 <template>
     <article class="ebooking">
         <section>
-            <p>Här är eran biljett till 
-                {{eBooked.cabin.name}}.</p>
+            <p>Här är eran biljett till
+                {{eBooked.name}}</p>
         </section>
         <section>
-            <p>Tillgång från: 
-                {{eBooked.cabin.date.from}}
-                 till 
-                 {{eBooked.cabin.date.to}}.</p>
+            <p>Tillgång från:
+                {{eBooked.date.from}}
+                 till
+                 {{eBooked.date.to}}.</p>
             <p>Incheckning sker
-                {{eBooked.cabin.date.checkin}} 
+                {{eBooked.date.checkin}}
                 och ni måste senast ha utcheckat till
-                {{eBooked.cabin.date.checkout}}</p>   
+                {{eBooked.date.checkout}}</p>
         </section>
         <section>
-            <p>{{eBooked.cabin.info}}</p>   
+            <p>{{eBooked.cabin.info}}</p>
             <p>Observera att erat referens nummer är
             <strong>{{eBooked.code}}</strong></p><p><em>
-            Ha det alltid till hands när ni ska 
+            Ha det alltid till hands när ni ska
             checka in och checka ut</em></p>
 
         </section>
